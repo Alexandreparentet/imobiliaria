8b34ca9c5731d0cdfc1c5948c6ebab39b6ca1565
unknown <ALEXANDRE6071@digitalcollegesul.local>
2024-10-15 15:57:35 -0300

    Estrutura Básica

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+// Simula um JSON online (Banco de Dados)
+const listaImoveis = [
+    {
+        id: 1,
+        titulo: 'Apart 3 dorm. Papicu',
+        descricao: 'Lista Aprtamento de 3 dormitórios localizado em um dos melhores bairros de Fortaleza.',
+        valor: 'R$ 500.000',
+        area: '90 m²',
+        quartos: 3,
+        tipos: 'aparatamento', // apart, casa, terreno
+        localizacao: 'Santos Dummont, Papicu',
+        venda_aluguel: 'venda', // aluguel
+        finalidade: 'residencial', // comercial
+        fotos: [
+            '',
+            '',
+            '',
+            '',
+            '',
+        ],
+    }
+];
