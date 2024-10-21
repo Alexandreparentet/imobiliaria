6df89a62579a51ac28ba1bcfba52086952d483c9
Alexandre Teixeira <alexandreparentet@gmail.com>
2024-10-21 13:25:53 -0300

    git de hoje

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,4 @@
+
 const formulario = document.querySelector('#formCadastro');
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
@@ -5,19 +6,22 @@ formulario.addEventListener('submit', (evento) => {
 });
 
 function cadastrarUsuario() {
-    
+
     // Busca a lista de usuários do Local Storage
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-    
+
     // Adiciona o usuário informado na lista
-    usuarios.push(
-        {
-            nome: formulario.nome.value,
-            email: formulario.email.value,
-            senha: formulario.senha.value
-        }
-    );
+    usuarios[0] = {
+        nome: formulario.nome.value,
+        email: formulario.email.value,
+        senha: formulario.senha.value
+    };
+
     // Salva os dados na Local Storage
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
     alert("Usuário cadastrado com sucesso!");
+
+    // Redireciona para a página Home
+    window.location.href = "../index.html";
 }
